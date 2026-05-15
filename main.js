f172ecfebabda43e067a89936462ca7dc35f1833
Essam Abdel Rahman Abdo Ahmed <78804035+essam73903@users.noreply.github.com>
2026-05-15 21:12:24 +0300

    Remove project settings comment from main.js

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
-// إعدادات مشروعك الجديد (Decent Atlas)
 const firebaseConfig = {
   apiKey: "AIzaSyAdPl4TmkXgeAD_PIY9dCgvifug-ez6WM",
   authDomain: "decent-atlas-310515.firebaseapp.com",
   projectId: "decent-atlas-310515",
   storageBucket: "decent-atlas-310515.firebasestorage.app",
   messagingSenderId: "388050632237",
-  appId: "1:388050632237:web:7e5efa0b93e016bf1cc2a1",
-  measurementId: "G-99V7W139DQ"
+  appId: "1:388050632237:web:7e5efa0b93e016bf1cc2a1"
 };
 
-// تشغيل Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
-// ربط المحفظة
+// عرض الرصيد
 db.collection("wallet").doc("balance_info").onSnapshot((doc) => {
     if (doc.exists) {
         document.getElementById("balance").innerText = doc.data().amount + " ريال";
