2b2c5a14d24804054121476b381bb3c1bf5cee70
Essam Abdel Rahman Abdo Ahmed <78804035+essam73903@users.noreply.github.com>
2026-05-15 20:49:25 +0300

    Update Firebase configuration settings

--- main.js
+++ main.js
@@ -1,23 +1,21 @@
-// بيانات مشروعك من Firebase Console
+// إعدادات مشروعك الجديد (Decent Atlas)
 const firebaseConfig = {
-    apiKey: "AIzaSyBoP272OND7bbvuJlR_BCGsc-5c-HjVAl8",
-    authDomain: "sam77-bef7d.firebaseapp.com",
-    projectId: "sam77-bef7d",
-    storageBucket: "sam77-bef7d.appspot.com",
-    messagingSenderId: "184354393850",
-    appId: "1:184354393850:web:718fb5a94980b00db64343"
+  apiKey: "AIzaSyAdPl4TmkXgeAD_PIY9dCgvifug-ez6WM",
+  authDomain: "decent-atlas-310515.firebaseapp.com",
+  projectId: "decent-atlas-310515",
+  storageBucket: "decent-atlas-310515.firebasestorage.app",
+  messagingSenderId: "388050632237",
+  appId: "1:388050632237:web:7e5efa0b93e016bf1cc2a1",
+  measurementId: "G-99V7W139DQ"
 };
 
-// تهيئة التطبيق
+// تشغيل Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
-// تحديث الرصيد مباشرة من Firestore
+// ربط المحفظة
 db.collection("wallet").doc("balance_info").onSnapshot((doc) => {
     if (doc.exists) {
         document.getElementById("balance").innerText = doc.data().amount + " ريال";
-        document.getElementById("status").innerText = "متصل بالسحاب ✅";
-    } else {
-        document.getElementById("status").innerText = "لم يتم إنشاء المحفظة بعد";
     }
 });
